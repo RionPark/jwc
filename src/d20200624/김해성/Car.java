1c3807c6be6d33418bafb924947c2ff65f54b1f3
riwiz <mr.riwiz@gmail.com>
2020-06-29 10:15:34 +0900

    김해성 숙제

---
+++ src/d20200624/김해성/Car.java
@@ -0,0 +1,27 @@
+package d20200624.김해성;
+
+public class Car {
+	private String drive;
+	private String name;
+	private int year;
+	private float engine;
+	private String color;
+	
+		
+	public void car(String drive,String name,int year,float engine, String color)	{
+		this.color = color;
+		this.name = name;
+		this.year = year;
+		this.engine = engine;
+		this.drive = drive;
+			
+		}
+	
+	public String toString()	{
+		return name+"  " + year+"  "+engine+"  "+color+"  "+drive;
+	}
+	
+	
+	
+	}
+
