3970733450c06e7d51efd86550522508cecc2973
lstem12 <lstem1217@gmail.com>
2020-06-29 09:57:47 +0900

    이상화 숙제

---
+++ src/d20200624/이상화/Book.java
@@ -0,0 +1,19 @@
+package d20200624.이상화;
+
+public class Book extends BookStore {
+	public void book(){
+		System.out.println("책이름 : "+bookName+"\r책가격 : "+bookPrice+"\r저자 :"+writer+"\r");
+	}
+	
+	public static void main(String[] args) {
+		BookStore b = new Book();
+		Book bs = new Book();
+		b.bookName = "개미";
+		b.bookPrice = 32000;
+		b.writer = "베르나르베르베르";
+		b.book();
+		bs.book();
+
+	}
+
+}
