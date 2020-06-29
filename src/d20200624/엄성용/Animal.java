d2a93a70b9713f82d29cdd905a4b7d176743104b
songjunghyun <yesparrot@gmail.com>
2020-06-29 10:11:16 +0900

    ㅇ

---
+++ src/d20200624/엄성용/Animal.java
@@ -0,0 +1,13 @@
+package d20200624.엄성용;
+
+public class Animal{
+	  void cry() {
+	  
+		  System.out.println("고양이가 울고있습니다");
+	  }
+	    
+	   
+	    
+}
+	   
+		
---
+++ src/d20200624/엄성용/Apple.java
@@ -0,0 +1,15 @@
+package d20200624.엄성용;
+
+public class Apple extends SmartPhone {
+
+	public void calling() {
+		System.out.println("전화받았습니다");
+	}
+
+	public static void main(String[] args) {
+		SmartPhone sm = new SmartPhone();
+		Apple a = new Apple();
+        sm.calling();
+        a.calling();
+	}
+}
