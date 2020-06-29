3e08486b024cb921d64c35aef7cdabbee55ba59d
dnjs5024 <dnjs7469@naver.com>
2020-06-29 09:54:04 +0900

    윤동제 숙제

---
+++ src/d20200624/윤동제/H_02.java
@@ -0,0 +1,10 @@
+package d20200624.윤동제;
+
+public class H_02 {
+	void Viscera() {
+		System.out.println("내 몸속 내장의 종류~");
+	}
+
+}
+
+
---
+++ src/d20200624/윤동제/H_04.java
@@ -0,0 +1,8 @@
+package d20200624.윤동제;
+
+public class H_04 {
+	protected void BuildingType() {
+		System.out.println("건물 종류");
+	}
+
+}
