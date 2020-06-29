ed20fcfdb9b7d9a96fa11f474a1ef9eaeef57764
Administrator <Administrator@Si7A-34>
2020-06-29 10:10:29 +0900

    ㄹㄹ

---
+++ src/d20200624/박윤상/Appletree.java
@@ -0,0 +1,19 @@
+package d20200624.박윤상;
+
+
+public class Appletree extends Tree{
+	
+	
+
+	
+	public static void main(String[]args) {
+		Appletree a1=new Appletree();
+		a1.water(1);
+		a1.apple();
+		a1.work();
+		
+	}
+		
+
+
+}
