728c664f2d02f15e61e24d3661e87f0f2c64552d
YoungkonSong <yksong0303@gmail.com>
2020-06-29 10:09:04 +0900

    가즈아~~~~~~~~

---
+++ src/d20200624/정원영/MegaCoffee.java
@@ -0,0 +1,17 @@
+package d20200624.정원영;
+
+public class MegaCoffee extends Coffee {
+	
+	public String getName() {
+		
+		return "Mega";
+	}
+	
+	public static void main(String[] args) {
+		
+		Coffee c = new MegaCoffee();
+		
+		System.out.println(c.getName());
+	}
+
+}
