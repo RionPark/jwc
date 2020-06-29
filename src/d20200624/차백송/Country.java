200e7799bc9ad86c570a18dc515f402b38a2cf52
qpnnm <qpnnm@naver.com>
2020-06-29 10:09:43 +0900

    가 즈 아 ~ ~ ~ ! ! ! Ga Z u A ! !! ! ! ! ! ! ! ! ! !digital city ! ! ! ㅋㅋ

---
+++ src/d20200624/차백송/Country.java
@@ -0,0 +1,13 @@
+package d20200624.차백송;
+
+public class Country extends World {
+	public void structure() {
+
+		System.out.println("건설합니다");
+	}
+
+	public static void main(String[] args) {
+		World w = new Country();
+		w.structure();
+	}
+}
