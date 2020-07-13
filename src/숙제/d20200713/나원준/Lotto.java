095d528c65e858c7b58b5b023f9d6454af396449
4679w <4679w@218.156.156.184>
2020-07-13 21:30:36 +0900

    제발

--- src/숙제/d20200713/나원준/Lotto.java
+++ src/숙제/d20200713/나원준/Lotto.java
@@ -3,24 +3,20 @@ package 숙제.d20200713.나원준;
 import java.util.Random;
 
 public class Lotto {
-	
-
-	
-		public static void main(String[] args) {
-			int[]numbers=new int[6]; 
-			Random r =new Random();
-			for(int i=0;i<numbers.length;i++) { 
-				numbers[i]=r.nextInt(45)+1;
-				for(int j = i - 1; j >= 0; j--) {
-					if(numbers[i]==numbers[j]) 
-						j--;
+	public static void main(String[] args) {
+		int nums[] = new int[6];
+		Random r = new Random();
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = r.nextInt(45) + 1;
+			for (int j = i - 1; j >= 0; j--) {
+				if (nums[i] == nums[j]) {
+					--i;
 				}
 			}
-			for(int i=0;i<numbers.length;i++)
-			System.out.println(numbers[i]);
+
 		}
-		
-
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
 	}
-
-
+}
