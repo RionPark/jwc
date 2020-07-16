f679069442c39cbc0ceadb1a3b13f660a1ec2d1e
hyunbae92 <hyunbae_kim@naver.com>
2020-07-16 16:12:40 +0900

    00

---
+++ src/숙제/d20200716/김현배/dao/MemberDAO.java
@@ -0,0 +1,13 @@
+package 숙제.d20200716.김현배.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MemberDAO {
+
+	int insertMember(Map<String,Object> member);
+	int updateMember(Map<String,Object> member);
+	int deleteMember(int mNum);
+	List<Map<String,Object>> selectMemberList(Map<String,Object> member);
+	Map<String,Object> selectMember(int mNum);
+}
